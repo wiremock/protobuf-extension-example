708295b65a5ccaa3697bb68717b22f74e85479dd
Tom Akehurst <tom@wiremock.org>
2026-04-23 14:29:37 +0100

    Shade dependencies in shadow JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.wiremock:wiremock:3.13.2")
+    compileOnly("org.wiremock:wiremock:3.13.2")
     implementation("com.google.protobuf:protobuf-java:3.25.8")
     implementation("com.google.protobuf:protobuf-java-util:3.25.8")
 
+    testImplementation("org.wiremock:wiremock:3.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.25.3")
@@ -40,6 +41,21 @@ protobuf {
                 "${project.layout.buildDirectory.get()}/resources/main/protobuf/descriptor.desc"
         }
     }
+}
+
+tasks.shadowJar {
+    relocate("com.google.protobuf", "shadow.com.google.protobuf")
+    relocate("google.protobuf", "shadow.google.protobuf")
+    relocate("com.google.gson", "shadow.com.google.gson")
+    relocate("com.google.code.gson", "shadow.com.google.code.gson")
+    relocate("com.google.common", "shadow.com.google.common")
+    relocate("com.google.thirdparty", "shadow.com.google.thirdparty")
+    relocate("javax.annotation", "shadow.javax.annotation")
+    relocate("com.google.errorprone", "shadow.com.google.errorprone")
+    relocate("com.google.j2objc", "shadow.com.google.j2objc")
+    relocate("org.checkerframework", "shadow.org.checkerframework")
+    relocate("org.jspecify", "shadow.org.jspecify")
+    relocate("com.google.guava", "shadow.com.google.guava")
 }
 
 tasks.test {
